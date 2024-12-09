2c360a5bb5756a38df37a53abef2b69bad60c4d0
Purname Yasa <yasapurnama@gmail.com>
2024-12-09 15:37:46 +0800

    add proxmox k3s

---
+++ main.tf
@@ -0,0 +1,140 @@
+resource "proxmox_vm_qemu" "master" {
+  count = var.vm_master_count
+  vmid  = var.vmid
+  name  = "${var.name}-master-${count.index}"
+
+  target_node = var.target_node
+  clone       = var.os_template
+  os_type     = var.os_type
+  agent       = var.agent
+
+  define_connection_info = true
+  ipconfig0 = var.ip_dhcp ? "ip=dhcp" : "ip=${var.ip_network_id}.${var.ip_network_host_master + count.index}/24,gw=${var.ip_gateway}"
+  nameserver = var.ip_dhcp ? "" : var.ip_dns_nameserver
+
+  cores    = var.cpu_cores
+  sockets  = var.cpu_socket
+  vcpus    = var.vcpus
+  memory   = var.memory
+  balloon  = var.balloon
+  hotplug  = var.hotplug
+  scsihw   = var.scsihw
+  bootdisk = var.bootdisk
+  onboot   = var.onboot
+  vm_state = var.vm_state
+
+  network {
+    model    = var.network_model
+    bridge   = var.network_bridge
+    tag      = var.network_tag
+    firewall = var.network_firewall
+  }
+
+  disks {
+    virtio {
+      virtio0 {
+        disk {
+          size   = var.disk_size
+          storage = var.disk_storage
+          discard = var.disk_discard
+        }
+      }
+    }
+    ide {
+      ide2 {
+        cloudinit {
+          storage = var.disk_storage
+        }
+      }
+    }
+  }
+
+  lifecycle {
+    ignore_changes = [
+      vmid,
+      ciuser,
+      sshkeys
+    ]
+  }
+
+}
+
+resource "proxmox_vm_qemu" "worker" {
+  count = var.vm_worker_count
+  vmid  = var.vmid
+  name  = "${var.name}-worker-${count.index}"
+
+  target_node = var.target_node
+  clone       = var.os_template
+  os_type     = var.os_type
+  agent       = var.agent
+
+  define_connection_info = true
+  ipconfig0 = var.ip_dhcp ? "ip=dhcp" : "ip=${var.ip_network_id}.${var.ip_network_host_worker + count.index}/24,gw=${var.ip_gateway}"
+  nameserver = var.ip_dhcp ? "" : var.ip_dns_nameserver
+
+  cores    = var.cpu_cores
+  sockets  = var.cpu_socket
+  vcpus    = var.vcpus
+  memory   = var.memory
+  balloon  = var.balloon
+  hotplug  = var.hotplug
+  scsihw   = var.scsihw
+  bootdisk = var.bootdisk
+  onboot   = var.onboot
+  vm_state = var.vm_state
+
+  network {
+    model    = var.network_model
+    bridge   = var.network_bridge
+    tag      = var.network_tag
+    firewall = var.network_firewall
+  }
+
+  disks {
+    virtio {
+      virtio0 {
+        disk {
+          size   = var.disk_size
+          storage = var.disk_storage
+          discard = var.disk_discard
+        }
+      }
+    }
+    ide {
+      ide2 {
+        cloudinit {
+          storage = var.disk_storage
+        }
+      }
+    }
+  }
+
+  lifecycle {
+    ignore_changes = [
+      vmid,
+      ciuser,
+      sshkeys
+    ]
+  }
+
+}
+
+resource "local_file" "inventory" {
+  filename = "ansible/inventory/k3s-cluster/hosts.ini"
+  content  = <<-EOT
+[master]
+%{ for key, vm in proxmox_vm_qemu.master ~}
+${vm.ssh_host}
+%{ endfor ~}
+
+[node]
+%{ for key, vm in proxmox_vm_qemu.worker ~}
+${vm.ssh_host}
+%{ endfor ~}
+
+[k3s_cluster:children]
+master
+node
+EOT
+}
